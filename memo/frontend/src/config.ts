3b764413e419cd5da8996e8d2469d541f0cecc9f
limengxun <limengxun45@outlook.com>
2022-10-06 23:05:05 +0800

    feat: frontend read domain-port config from url

--- memo/frontend/src/config.ts
+++ memo/frontend/src/config.ts
@@ -1,3 +1,6 @@
 
-export const DOMAINURL: string = 'http://localhost';
-export const PORT: string = '8888'
+// export const DOMAINURL: string = 'http://localhost';
+// export const PORT: string = '8888'
+
+export const DOMAINURL = "http://" + window.location.hostname;
+export const PORT = window.location.port;
