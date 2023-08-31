0632144b1810809e64300db046f428cbebd6a988
Li, mengxun <limengxun45@outlook.com>
2023-08-31 11:58:45 +0800

    feat: ssl

--- memo/frontend/src/config.ts
+++ memo/frontend/src/config.ts
@@ -2,5 +2,6 @@
 // export const DOMAINURL: string = 'http://localhost';
 // export const PORT: string = '8888'
 
-export const DOMAINURL = "http://" + window.location.hostname;
+const PROTOCOL = window.location.protocol;
+export const DOMAINURL = PROTOCOL + "//" + window.location.hostname;
 export const PORT = window.location.port;
--- memo/server.py
+++ memo/server.py
@@ -2,6 +2,7 @@ import asyncio, os, argparse, logging
 import tornado.web
 import tornado.autoreload
 from tornado.routing import _RuleList
+from tornado.httpserver import HTTPServer, HTTPServer
 from .backend.handlers import AuthHandler, IndexHandler, MemoHandler
 
 __this_dir = os.path.dirname(__file__)
@@ -12,8 +13,7 @@ logger = logging.getLogger("memo")
 def autoreloadHook():
     logger.info("Restart server.")
 
-def make_app():
-
+def make_app(ssl_options=None):
     rules: _RuleList = [
         (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": frontend_root}),
         (r"^/backend/auth", AuthHandler),
@@ -28,12 +28,19 @@ def make_app():
 
     return tornado.web.Application(
         rules,
+        ssl_options=ssl_options
     )
 
-async def main(port: int):
-    app = make_app()
+async def main(port: int, ssl_options=None):
+    app = make_app(ssl_options)
     logger.info(f"Starting server at port: {port}")
-    app.listen(port)
+    if ssl_options:
+        logger.info(f"Start with ssl options: {ssl_options}")
+        server = HTTPServer(app, ssl_options=ssl_options)
+    else:
+        server = HTTPServer(app)
+
+    server.listen(port)
     tornado.autoreload.add_reload_hook(autoreloadHook)
     tornado.autoreload.start()
     await asyncio.Event().wait()
@@ -41,6 +48,18 @@ async def main(port: int):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Memo server")
     parser.add_argument("-p", "--port", type=int, default=8888)
+    parser.add_argument("--ssl-keyfile", type=str, default="")
+    parser.add_argument("--ssl-certfile", type=str, default="")
     args = parser.parse_args()
 
-    asyncio.run(main(args.port))
+    ssl_options = None
+
+    if bool(args.ssl_keyfile) != bool(args.ssl_certfile):
+        logger.error("ssl-keyfile and ssl-certfile must be both specified or both unspecified")
+        exit(1)
+    
+    if args.ssl_keyfile and args.ssl_certfile:
+        ssl_options = {"certfile": args.ssl_certfile,
+                       "keyfile": args.ssl_keyfile}
+
+    asyncio.run(main(args.port, ssl_options))
